9085503b1e040b68557f2eb6d2e1caac4867517a
Sergei Repnikov <theierus@gmail.com>
2020-10-07 18:44:40 +0300

    re-wrote in functional style

--- src/main/scala/rpnkv/iv/Second.scala
+++ src/main/scala/rpnkv/iv/Second.scala
@@ -1,24 +1,40 @@
 package rpnkv.iv
 
 object Second {
-  def switchesCount(list: List[Integer]): Integer = {
+  def switchesCount(list: List[Int]): Int = {
     val listNoZeros = list.filter(notZero)
     listNoZeros
       .headOption match {
       case Some(head) =>
-        listNoZeros.take(1)
-          .foldLeft(0 -> head)(merge)
-        0
-
+        listNoZeros.foldLeft(0 -> head)(merge)._1
       case None => 0
     }
   }
 
-  def merge(prev: (Integer, Integer), current: Integer): (Integer ,Integer) = {
-    Integer(0) -> Integer(0)
+  def merge(prev: (Int, Int), current: Int): (Int, Int) = {
+    val counter =
+      if (prev._2 > 0) {
+        if (current < 0) {
+          prev._1 + 1
+        } else {
+          prev._1
+        }
+      } else {
+        if (prev._2 < 0) {
+          if (current > 0) {
+            prev._1 + 1
+          } else {
+            prev._1
+          }
+        } else {
+          prev._1
+        }
+      }
+
+    (counter -> current)
   }
 
-  def notZero(int: Integer): Boolean = int != 0
+  def notZero(int: Int): Boolean = int != 0
 
   def pair: (String, String) = {
     "x" -> "x"
